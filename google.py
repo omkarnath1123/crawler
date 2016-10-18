9261e0c0eb42af21e87847f4b4e2aac06557c834
omkarnath1123 <omkarnath1123@gmail.com>
2016-10-18 22:19:22 +0530

    scrape project

---
+++ google.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import urllib
+import urlparse
+import lxml.html
+from downloader import Downloader
+
+
+def search(keyword):
+    D = Downloader()
+    url = 'https://www.google.com/search?q=' + urllib.quote_plus(keyword)
+    html = D(url)
+    tree = lxml.html.fromstring(html)
+    links = []
+    for result in tree.cssselect('h3.r a'):
+        link = result.get('href')
+        qs = urlparse.urlparse(link).query
+        links.extend(urlparse.parse_qs(qs).get('q', []))
+    return links
+
+def scrape(html):
+    tree = lxml.html.fromstring(html)
+    td = tree.cssselect('tr#places_neighbours__row > td.w2p_fw')[0]
+    area = td.text_content()
+    return area
+    
+if __name__ == '__main__':
+    try:
+        keyword = sys.argv[1]
+    except IndexError:
+        keyword = 'test'
+    print search(keyword)
